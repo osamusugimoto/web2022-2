367d578a8482dcf3e7f59b5fb76af69c807e82e2
Osamu Sugimoto <sam@josai.ac.jp>
2022-10-11 10:41:17 +0900

    Update ex06-6.js

--- ex06-6.js
+++ ex06-6.js
@@ -3,5 +3,5 @@ function sayhello66() {alert('Hello, ' + who66 + '!');
                       }
 function taro66() {who66 = '太郎'; alert(who66 + 'が来た！');
                   }
-function hanako65() {who65 = '花子'; alert(who66 + 'が来た！');
+function hanako65() {who66 = '花子'; alert(who66 + 'が来た！');
                     }
