dd0142a14c36422636ab8175bba56de29ce513c3
Osamu Sugimoto <sam@josai.ac.jp>
2022-10-31 18:53:57 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,1 +1,1 @@
-function gazo688() {document.getElementById('who688').innerHTML= './image/bulbon.gif';}
+function gazo688() {document.getElementById('who688').innerHTML= 'img src="./image/bulbon.gif"';}
