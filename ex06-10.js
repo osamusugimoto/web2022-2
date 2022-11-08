c10d1178e939db49c5de723ec5a6b02fd9d0f38f
Osamu Sugimoto <sam@josai.ac.jp>
2022-11-08 10:40:57 +0900

    Create ex06-10.js

---
+++ ex06-10.js
@@ -0,0 +1,2 @@
+function imgchange1() {document.getElementById('bulboff').src= './image/bulbon.gif';
+function imgback1() {document.getElementById('bulbon').src= './image/bulboff.gif';                       
