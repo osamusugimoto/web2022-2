0993db77084cb704ea5b61410b7245f05a5e0762
Osamu Sugimoto <sam@josai.ac.jp>
2022-11-15 10:08:49 +0900

    Create ex6-10-2.js

---
+++ ex6-10-2.js
@@ -0,0 +1,3 @@
+function imgchange1() {document.getElementById('size').src= './image/josai_new.png';}
+                       
+function imgback1() {document.getElementById('size').src='./image/keiei_small.jpg';}
