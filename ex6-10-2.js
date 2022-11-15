2a33798dc4bba0fb87eb282df7db88292007a424
Osamu Sugimoto <sam@josai.ac.jp>
2022-11-15 10:23:02 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,3 +1,3 @@
-function imgchange1() {document.getElementById('size').src= './image/josai_new.png';}
+function imgchange2() {document.getElementById('size').src= './image/josai_new.png';}
                        
-function imgback1() {document.getElementById('size').src='./image/keiei_small.jpg';}
+function imgback2() {document.getElementById('size').src='./image/keiei_small.jpg';}
