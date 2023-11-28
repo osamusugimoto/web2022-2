8bc1527dbf8ae078995861e9b796ca34e625991e
Osamu Sugimoto <sam@josai.ac.jp>
2023-11-28 10:35:38 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,3 @@
-function imgchange2() {document.getElementById('bulb2').src= './image/bulbon.gif'; setTimeout('imgback2()',3000);}
+function imgchange3() {document.getElementById('bulb2').src= './image/bulbon.gif'; setTimeout('imgback3()',3000);}
 
-function imgback2() {document.getElementById('bulb2').src='./image/bulboff.gif';}
+function imgback3() {document.getElementById('bulb2').src='./image/bulboff.gif';}
